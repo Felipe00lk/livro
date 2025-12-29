c0725c703dd89c18f0a650bbb2fce029cff428c5
Felipe00lk <felipecruzfc185@gmail.com>
2025-12-29 01:07:59 -0300

    oo

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
 const folhas = document.querySelectorAll(".folha");
+const btnAvancar = document.getElementById("avancar");
+const btnVoltar = document.getElementById("voltar");
+
 let folhaAtual = 0;
 
-avancar.addEventListener("click", () => {
+btnAvancar.addEventListener("click", () => {
   if (folhaAtual < folhas.length) {
     folhas[folhaAtual].classList.add("virada");
     folhaAtual++;
   }
 });
 
-voltar.addEventListener("click", () => {
+btnVoltar.addEventListener("click", () => {
   if (folhaAtual > 0) {
     folhaAtual--;
     folhas[folhaAtual].classList.remove("virada");
