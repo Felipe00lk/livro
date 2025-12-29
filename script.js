20e635396aa0bf38100807aa7929354c0b64b22b
Felipe00lk <felipecruzfc185@gmail.com>
2025-12-29 00:51:02 -0300

    oi

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const paginas = document.querySelectorAll(".pagina");
+const btnAvancar = document.getElementById("avancar");
+const btnVoltar = document.getElementById("voltar");
 
+let paginaAtual = 0;
+
+function mostrarPagina(index) {
+  paginas.forEach((pagina, i) => {
+    pagina.classList.remove("ativa");
+    if (i === index) {
+      pagina.classList.add("ativa");
+    }
+  });
+}
+
+btnAvancar.addEventListener("click", () => {
+  if (paginaAtual < paginas.length - 1) {
+    paginaAtual++;
+    mostrarPagina(paginaAtual);
+  }
+});
+
+btnVoltar.addEventListener("click", () => {
+  if (paginaAtual > 0) {
+    paginaAtual--;
+    mostrarPagina(paginaAtual);
+  }
+});
